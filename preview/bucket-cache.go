b90d7dfe8b58513f01e6d6369369e9e3a5942cd0
Andrew O'Neill <foolusion@gmail.com>
2017-05-02 20:55:47 -0700

    pretty sure this will fix it

--- preview/bucket-cache.go
+++ preview/bucket-cache.go
@@ -5,6 +5,9 @@ import (
 	"context"
 	"io"
 	"io/ioutil"
+	"sync"
+
+	"golang.org/x/crypto/acme/autocert"
 
 	"cloud.google.com/go/storage"
 	"github.com/pkg/errors"
@@ -13,6 +16,8 @@ import (
 type bucket struct {
 	name string
 	*storage.Client
+	cert []byte
+	mu   sync.RWMutex
 }
 
 func newBucket(ctx context.Context, name, project string) (*bucket, error) {
@@ -24,8 +29,15 @@ func newBucket(ctx context.Context, name, project string) (*bucket, error) {
 }
 
 func (b *bucket) Get(ctx context.Context, key string) ([]byte, error) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	if b.cert != nil {
+		return b.cert, nil
+	}
 	r, err := b.Bucket(b.name).Object(key).NewReader(ctx)
-	if err != nil {
+	if err == storage.ErrObjectNotExist {
+		return nil, autocert.ErrCacheMiss
+	} else if err != nil {
 		return nil, errors.Wrap(err, "could not create reader on bucket")
 	}
 	return ioutil.ReadAll(r)
@@ -36,9 +48,17 @@ func (b *bucket) Put(ctx context.Context, key string, data []byte) error {
 	if _, err := io.Copy(w, bytes.NewBuffer(data)); err != nil {
 		return errors.Wrap(err, "could not write to object")
 	}
+	b.mu.Lock()
+	b.cert = data
+	b.mu.Unlock()
 	return nil
 }
 
 func (b *bucket) Delete(ctx context.Context, key string) error {
-	return b.Bucket(b.name).Object(key).Delete(ctx)
+	if err := b.Bucket(b.name).Object(key).Delete(ctx); err == storage.ErrObjectNotExist {
+		return nil
+	} else if err != nil {
+		return errors.Wrap(err, "could not delete object")
+	}
+	return nil
 }
